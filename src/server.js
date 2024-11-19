4ff90800a16fc65b425e534034e6e4d4216e7654
hi <150579399+isafcck@users.noreply.github.com>
2024-11-19 19:24:59 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,29 @@
 
+const http = require('http');
+const { Worker } = require('worker_threads');
+const config = require('../config/config');
+const router = require('../src/router');
+
+let workers = [];
+
+for (let i = 0; i < config.NUM_WORKERS; i++) {
+  const worker = new Worker('./src/workers/worker.js');
+  workers.push(worker);
+}
+
+const server = http.createServer((req, res) => {
+  const worker = workers.shift();
+  workers.push(worker);
+
+  worker.once('message', (response) => {
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.end(response);
+  });
+
+  worker.postMessage({ url: req.url, method: req.method, router: router });
+});
+
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
+                                 
